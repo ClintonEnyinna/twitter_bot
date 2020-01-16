1a7d220713c8de2735b3bd3530cee03273e369c3
ClintonEnyinna <enyinnaclinton@gmail.com>
2020-01-15 23:42:03 -0600

    Correct rubocop errors

--- lib/user.rb
+++ lib/user.rb
@@ -7,7 +7,7 @@ class User
     @username = username
     @password = password
     @logged_in = false
-    @browser = "firefox"
+    @browser = 'firefox'
     @driver = nil
   end
 
@@ -19,7 +19,7 @@ class User
 
   def login
     @driver = Selenium::WebDriver.for @browser.to_sym
-    url = "https://twitter.com/login"
+    url = 'https://twitter.com/login'
 
     go_to(url)
     fill_info
@@ -31,27 +31,26 @@ class User
 
   def go_to(url)
     until @logged_in
-      @driver.navigate.to(url)
-      inputs = @driver.find_element(xpath: "//div[contains(@class, 'field')]//input[@name='session[username_or_email]']")
       begin
         @driver.navigate.to(url)
         sleep 3
-        @logged_in = @driver.find_element(xpath: "//div[contains(@class, 'field')]//input[@name='session[username_or_email]']").displayed?
-      rescue
-        puts "reconnecting..."
+        search_pattern = "//div[contains(@class, 'field')]//input[@name='session[username_or_email]']"
+        @logged_in = @driver.find_element(xpath: search_pattern).displayed?
+      rescue StandardError
+        puts 'reconnecting...'
         sleep 5
       end
     end
   end
 
   def fill_info
-    username_input = @driver.find_element(xpath: "//div[contains(@class, 'field')]//input[@name='session[username_or_email]']")
-    username_input.clear
-    username_input.send_keys(@username)
+    usr_inp = @driver.find_element(xpath: "//div[contains(@class, 'field')]//input[@name='session[username_or_email]']")
+    usr_inp.clear
+    usr_inp.send_keys(@username)
 
-    password_input = @driver.find_element(xpath: "//div[contains(@class, 'field')]//input[@name='session[password]']")
-    password_input.clear
-    password_input.send_keys(@password)
-    password_input.send_keys(:enter)
+    password_inp = @driver.find_element(xpath: "//div[contains(@class, 'field')]//input[@name='session[password]']")
+    password_inp.clear
+    password_inp.send_keys(@password)
+    password_inp.send_keys(:enter)
   end
 end
